63c9c8b329f19bca21ddf1886f0a153b81f19c61
zachath <zachariasthorell@gmail.com>
2022-02-19 09:08:42 +0100

    Comments and method declarations

--- src/CWA.java
+++ src/CWA.java
@@ -1,8 +1,67 @@
-import java.util.Iterator;
+//Michael Foussianis and Zacharias Thorell
+//2022
 
+import java.util.Iterator;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+/**
+ * Self implemented version of the CopyOnWriteArrayList.
+ * Implements only the Iterable interface since the only operations of interest are modification (add/remove) and
+ * iteration.
+ * Based upon: {@link java.util.concurrent.CopyOnWriteArrayList}
+ * OpenJDK GitHub: https://github.com/openjdk/jdk/blob/master/src/java.base/share/classes/java/util/concurrent/CopyOnWriteArrayList.java
+ * @param <T> Type the CWA contains.
+ */
 public class CWA<T> implements Iterable<T> {
+
+    //Private intrinsic lock.
+    private final Object lock = new Object();
+
+    //The array of objects, volatile to ensure that updates propagate predictably to other threads.
+    private volatile Object[] array;
+
+    public CWA() {
+        throw new IllegalStateException("Not implemented");
+    }
+
+    /**
+     * Adds the specified element to the list by locking it from other threads and adding to a "fresh copy of the array"
+     * as not to interrupt operations of other threads.
+     *
+     * @param element to be added to the list.
+     * @return true.
+     */
+    public boolean add(T element) {
+        throw new IllegalStateException("Not implemented");
+    }
+
+    /**
+     * Removes the element of the specified index in the list by locking it from other threads and removing from a
+     * "fresh copy" of the array as not to interrupt operations of other threads.
+     *
+     * @param index of the element to remove.
+     * @return the element that was removed from the list.
+     * @throws IndexOutOfBoundsException if index lies outside the bounds of the list.
+     */
+    public T remove(int index) {
+        throw new IllegalStateException("Not implemented");
+    }
+
     @Override
     public Iterator<T> iterator() {
-        throw new IllegalStateException("Not implemented");
+        return new CWAIterator();
+    }
+
+    private class CWAIterator implements Iterator<T> {
+
+        @Override
+        public boolean hasNext() {
+            throw new IllegalStateException("Not implemented");
+        }
+
+        @Override
+        public T next() {
+            throw new IllegalStateException("Not implemented");
+        }
     }
 }
