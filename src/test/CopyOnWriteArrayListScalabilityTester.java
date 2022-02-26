eca4e1cc4f28d8f4fe124ea672dc721bb1745d5d
zachath <zachariasthorell@gmail.com>
2022-02-26 08:35:15 +0100

    comments and median and average calulcations for TestResult.

--- src/test/CopyOnWriteArrayListScalabilityTester.java
+++ src/test/CopyOnWriteArrayListScalabilityTester.java
@@ -3,7 +3,11 @@
 
 package test;
 
-import java.util.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
@@ -24,6 +28,7 @@ public class CopyOnWriteArrayListScalabilityTester {
     //Runtime of tests
     private final static int runTime = 10000;
 
+    //Stores all test results, reset after warmup.
     private static List<TestResult> testResults = new ArrayList<>();
 
     //The operations to be performed.
@@ -106,27 +111,42 @@ public class CopyOnWriteArrayListScalabilityTester {
         for (int i = 0; i < removePercentage; i++) {
             operations.add(Operations.REMOVE);
         }
+
         Collections.shuffle(operations);
         return operations;
     }
 
+    /**
+     * The entire test program is run once to ensure dynamic compilation, results are discarded and test reset afterwards.
+     */
     private static void warmup() {
         runTest();
     }
 
+    /**
+     * Resets the tests by creating a new instance of the CWA, calling garbage collection and setting the testIsFinished
+     * flag to true.
+     */
     private static void resetDataStructure() {
         CWA = new CopyOnWriteArrayList<>();
         System.gc();
         testIsFinished = false;
     }
 
+    /**
+     * Runs as many tests specified by the testIterations variable.
+     * The CWA is populated with as many elements as specified,
+     * then workers and threads are created then started.
+     * They run for runTime milliseconds before the testIsFinished flag is set
+     * to false and the threads terminate. The amount of totalOperations is tallied up and
+     * a TestResult instance is created for every test. Finally, the test is reset.
+     */
     private static void runTest() {
         int totalOperations = 0;
 
         for (int i = 0; i < testIterations; i++) {
             System.out.println("Test " + i);
 
-            //Har innehållet någon påverkan?
             for (int j = 0; j < numberOfElements; j++) {
                 CWA.add(j);
             }
@@ -162,12 +182,15 @@ public class CopyOnWriteArrayListScalabilityTester {
 
             System.out.println("Test [" + i + "] Complete");
 
-            testResults.add(new TestResult(testName, totalOperations, totalTime, numberOfThreads, numberOfElements));
+            testResults.add(new TestResult(totalOperations, totalTime, numberOfThreads, numberOfElements));
 
             resetDataStructure();
         }
     }
 
+    /**
+     * Performs the work in the created threads.
+     */
     private static class Worker implements Runnable {
         private final List<Operations> operations;
         private final Random random;
@@ -178,28 +201,45 @@ public class CopyOnWriteArrayListScalabilityTester {
             this.random = new Random();
         }
 
+        /**
+         * Gets the element specified by index.
+         * to avoid dead code elimination the hashcode of value is compared to
+         * the current time.
+         * @param index of the element to lookup.
+         */
         private void lookup(int index) {
             Integer value = CWA.get(index);
 
-            //Dead code elimination as per Goetz et. al 2007
+            //Dead code elimination as per Goetz et. al 2006
             if (value.hashCode() == System.nanoTime()) {
                 System.out.print(" ");
             }
         }
 
+        /**
+         * Iterates through the CWA without doing anything,
+         * to avoid dead code elimination the hashcode of i is compared to
+         * the current time.
+         */
         private void iterate() {
             for (Integer i : CWA) {
-                //Dead code elimination as per Goetz et. al 2007
+                //Dead code elimination as per Goetz et. al 2006
                 if (i.hashCode() == System.nanoTime()) {
                     System.out.print(" ");
                 }
             }
         }
 
+        /**
+         * Performs the operation specified by operations and count the total amount of operations until
+         * testIsFinished is set to true by the main thread.
+         */
         @Override
         public void run() {
             while (!testIsFinished) {
+                //Iterates through the list using modulus.
                 Operations operation = operations.get(totalOperations % operations.size());
+
                 int randomValue = random.nextInt(operations.size() - 1);
 
                 if (operation == Operations.LOOKUP) {
--- src/test/TestResult.java
+++ src/test/TestResult.java
@@ -2,19 +2,51 @@
 
 package test;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Container class for the results of tests.
  */
-public class TestResult {
-    private final String testName;
-    private final int totalOperations, threadCount, elementCount;
-    private final long throughput;
+public class TestResult implements Comparable<TestResult> {
+    public final int totalOperations, threadCount, elementCount;
+    public final long throughput;
 
-    public TestResult(String testName, int totalOperations, long totalTime, int threadCount, int elementCount) {
-        this.testName = testName;
+    public TestResult(int totalOperations, long totalTime, int threadCount, int elementCount) {
         this.totalOperations = totalOperations;
         this.threadCount = threadCount;
         this.elementCount = elementCount;
         this.throughput = this.totalOperations / totalTime;
+    }
+
+    /**
+     * Returns the average throughput of the list.
+     * @param resultList as input.
+     * @return average of resultList.
+     */
+    public static long getAverageThroughput(List<TestResult> resultList) {
+        long total = 0;
+        for (TestResult result : resultList) {
+            total += result.throughput;
+        }
+        return total / resultList.size();
+    }
+
+    /**
+     * Returns the median throughput of the list.
+     * @param resultList as input.
+     * @return median of resultList.
+     */
+    public static long getMedianThroughput(List<TestResult> resultList) {
+        List<TestResult> sortedList = new ArrayList<>(resultList);
+        Collections.sort(sortedList);
+
+        return (sortedList.get(sortedList.size()/2).throughput + sortedList.get(sortedList.size()/2 - 1).throughput)/2;
+    }
+
+    @Override
+    public int compareTo(TestResult other) {
+        return Long.compare(this.throughput, other.throughput);
     }
 }
