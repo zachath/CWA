694f661a70d54424d3ca6d3909dde8ad68feef93
zachath <zachariasthorell@gmail.com>
2022-03-11 09:57:47 +0100

    An interface defining the subset of methods.

---
+++ src/CWASubset.java
@@ -0,0 +1,8 @@
+//Interface defining the functionality of the subset this thesis is working with.
+
+public interface CWASubset<T> extends Iterable<T> {
+    T get(int index);
+    int size();
+    boolean add(T element);
+    T remove(int index);
+}
